aa6a0b3607779ab88c1ed2c9d1567616610299e9
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-03-25 15:40:14 +0300

    fix: implement environments for dynaconf
    make possible to use dev and prod environments

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,7 +6,12 @@ from pathlib import Path
 
 from dynaconf import LazySettings
 
+
 config = LazySettings(
     ROOT_PATH_FOR_DYNACONF=Path(os.path.abspath(__file__)).parent,
-    PRELOAD_FOR_DYNACONF=['config/*'],
+    SETTINGS_FILE_FOR_DYNACONF="settings.yaml",
+    ENV_SWITCHER_FOR_DYNACONF='KREOSHINE_ENV',  # `export KREOSHINE_ENV=target_environment` in .env file
+    YAML_LOADER_FOR_DYNACONF='safe',
+    environments=True,  # activate layered environments
+    load_dotenv=True,  # read a .env file
 )
