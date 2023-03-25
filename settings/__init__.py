3f260557dc5747f01e85425ff409a6ca59e58b3f
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-03-25 23:24:55 +0300

    chore: define root dir for dynaconf as constant

--- settings/__init__.py
+++ settings/__init__.py
@@ -6,9 +6,10 @@ from pathlib import Path
 
 from dynaconf import LazySettings
 
+DYNACONF_ROOT_PATH = Path(os.path.abspath(__file__)).parent
 
 config = LazySettings(
-    ROOT_PATH_FOR_DYNACONF=Path(os.path.abspath(__file__)).parent,
+    ROOT_PATH_FOR_DYNACONF=DYNACONF_ROOT_PATH,
     SETTINGS_FILE_FOR_DYNACONF="settings.yaml",
     ENV_SWITCHER_FOR_DYNACONF='KREOSHINE_ENV',  # `export KREOSHINE_ENV=target_environment` in .env file
     YAML_LOADER_FOR_DYNACONF='safe',
