c0b70993bea23d6a9ea35af0207340c0e9edc7e3
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-03-26 09:50:07 +0300

    refactor: split 'dev' and 'prod' settings

--- settings/__init__.py
+++ settings/__init__.py
@@ -10,7 +10,7 @@ DYNACONF_ROOT_PATH = Path(os.path.abspath(__file__)).parent
 
 config = LazySettings(
     ROOT_PATH_FOR_DYNACONF=DYNACONF_ROOT_PATH,
-    SETTINGS_FILE_FOR_DYNACONF="settings.yaml",
+    SETTINGS_FILE_FOR_DYNACONF=['config/prod-settings.yaml', 'config/dev-settings.yaml'],
     ENV_SWITCHER_FOR_DYNACONF='KREOSHINE_ENV',  # `export KREOSHINE_ENV=target_environment` in .env file
     YAML_LOADER_FOR_DYNACONF='safe',
     environments=True,  # activate layered environments
