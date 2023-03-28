e99c10c1477f6610aadcf22c6294a08939e08b71
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-03-29 00:32:31 +0300

    refactor: move service initialization logic in init module

--- app/backend/services/main/__init__.py
+++ app/backend/services/main/__init__.py
@@ -1,49 +1,60 @@
 """
 Main KreoShine service
 """
+import logging
 import logging.config
+import os
+import sys
 
 from concurrent.futures import ThreadPoolExecutor
 
 import asyncio
 from aiohttp import web
 
-logger = logging.getLogger('main')
+from deploy import DEVELOPMENT_MODE, TEMPORARY_DIR
+from settings import config
+
+logger = logging.getLogger('main_service')
+
+
+def handle_exception(exc_type, exc_value, exc_traceback) -> None:
+    """ Handler for uncaught exceptions for 'main_service' logger """
+    if issubclass(exc_type, KeyboardInterrupt):
+        sys.__excepthook__(exc_type, exc_value, exc_traceback)
+        return
+    logger.critical("Uncaught exception!", exc_info=(exc_type, exc_value, exc_traceback))
+
+
+def configure_logging(logging_config: dict) -> None:
+    """ Configures logging for 'main_service' service """
+    if config.deploy.mode == DEVELOPMENT_MODE:
+        logging_config['handlers']['service_file']['filename'] = os.path.join(TEMPORARY_DIR, 'main-service.log')
+    logging.config.dictConfig(config=logging_config)
+    sys.excepthook = handle_exception
 
 
 async def on_app_start(app):
-    """
-    Service initialization on application start
-
-    Args:
-        app: instance of the application
-    """
-    service_config = app['service_config']
+    """ Service initialization on application start """
+    service_config = config.app.services.main
     asyncio.get_event_loop().set_default_executor(ThreadPoolExecutor(max_workers=service_config['thread_pool_size']))
 
-    logger.info('Init Database engine')
-    # todo: implement database initialization
+    logger.debug('Initialize database engine')
+    # todo: implement database initialization (if need)
+    logger.info("Service 'main' successfully started")
 
 
 async def on_app_stop(app) -> None:
-    """
-    Stops tasks on the application destroy
-
-    Args:
-        app: instance of the application
-    """
+    """ Stops tasks on the application destroy """
 
 
-def create_app(service_config: dict) -> web.Application:
-    """
-    Creates web application
+def create_main_app() -> web.Application:
+    """ Creates instance of web application for 'main' service """
+    configure_logging(logging_config=config.logging_main_service)
 
-    Args:
-        service_config: configuration for service
-    """
-    app = web.Application(client_max_size=service_config['client_max_size'])
-    app['service_config'] = service_config
+    app = web.Application(client_max_size=config.app.services.main['client_max_size'])
+
     app.on_startup.append(on_app_start)
     app.on_shutdown.append(on_app_stop)
 
+    logger.debug("Instance of application for 'main' service successfully created")
     return app
