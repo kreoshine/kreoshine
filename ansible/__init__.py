d28c43ff242eace89adfe86095b09ce3c8cc8b6c
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-04-08 23:58:36 +0300

    style: fix pylint

--- ansible/__init__.py
+++ ansible/__init__.py
@@ -5,6 +5,7 @@ from ansible.abstract_executor import AbstractAnsibleExecutor
 from ansible.mixins import PlaybookExecutorMixin, ModuleExecutorMixin
 
 
+# pylint: disable = too-few-public-methods
 class AnsibleExecutor(AbstractAnsibleExecutor, PlaybookExecutorMixin, ModuleExecutorMixin):
     """
     Represents async methods for running different tasks via ansible-runner
@@ -14,7 +15,6 @@ class AnsibleExecutor(AbstractAnsibleExecutor, PlaybookExecutorMixin, ModuleExec
         super().__init__(private_data_dir, verbosity)
         self._host_pattern = host_pattern
 
-    # pylint: disable = too-few-public-methods
     @property
     def target_host_pattern(self) -> str:
         """ Target host pattern for which ansible executor is running """
