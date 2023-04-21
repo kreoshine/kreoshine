02448ca2f6ea27b2e7c4c5e884285ee19bde29b8
pillager-SB <pillager@bk.ru>
2023-04-21 21:59:30 +0300

    6+

---
+++ backend/alembic/versions/1574e72b7af2_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 1574e72b7af2
+Revises: 
+Create Date: 2023-04-21 21:16:20.509765
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '1574e72b7af2'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
