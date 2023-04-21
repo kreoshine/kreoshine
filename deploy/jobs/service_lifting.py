786b7df84541a06d7d33b5e5ed3be51ee8c3929f
Sintsov Vitaliy <sincov.vitalii@gmail.com>
2023-04-21 15:34:06 +0300

    refactor: use another way to define path to static dir

--- deploy/jobs/service_lifting.py
+++ deploy/jobs/service_lifting.py
@@ -4,6 +4,7 @@ Jobs that lift different services
 import logging
 
 from ansible import AnsibleExecutor
+from deploy.deploy_const import PROJECT_ROOT_PATH
 
 logger = logging.getLogger('ansible_deploy')
 
@@ -14,6 +15,7 @@ async def init_nginx_container(ansible: AnsibleExecutor) -> None:
         ansible: instance of ansible executor
     """
     logger.debug("Nginx configuration on a %s host", ansible.target_host_pattern)
-    nginx_initialization_task = ansible.ansible_roles.execute_nginx_role()
+    nginx_initialization_task = ansible.ansible_roles.execute_nginx_role(
+        local_root_to_static=str(PROJECT_ROOT_PATH.joinpath('frontend/')))
     await nginx_initialization_task
     logger.info("Successfully create nginx container")
