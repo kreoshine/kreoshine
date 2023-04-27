939d64a1d8a63337f1a6ec221dc055793ce9b57c
pillager-SB <pillager@bk.ru>
2023-04-27 17:41:21 +0300

    fix: model simplification

--- backend/db/models_configuration/models.py
+++ backend/db/models_configuration/models.py
@@ -1,11 +1,19 @@
 # pylint: disable=C0114,E1102,R0903, E0213
 from __future__ import annotations
 import datetime
+import enum
+from typing import Literal
+from sqlalchemy.dialects.postgresql import ARRAY
 from sqlalchemy import ForeignKey
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy import String
+from sqlalchemy.orm import mapped_column
+import sqlalchemy
+
+Status = Literal["received", "discussed", "in progress", "closed"]
+VStatus = Literal["visible", "invsible"]
 
 
 class Base(DeclarativeBase):
@@ -18,15 +26,14 @@ class Base(DeclarativeBase):
         server_default=func.now(), default=datetime.datetime.now())
 
 
-class Image(Base):
+class ImageGroups(Base):
     """
-    Единая таблица изобржений.
+    Таблица групп изображений
     """
-    __tablename__ = "image"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(nullable=False)
-    description: Mapped[str] = mapped_column(nullable=True)
-    url: Mapped[str] = mapped_column(nullable=False)
+    __tablename__ = "imagegroups"
+    name: Mapped[str] = mapped_column(
+        primary_key=True, nullable=False, unique=True)
+    url_group: Mapped[list[str]] = mapped_column(ARRAY(String))
 
 
 class Service(Base):
@@ -37,8 +44,10 @@ class Service(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
-
     short_deskription: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[VStatus] = mapped_column(sqlalchemy.Enum(
+        "visible", "invsible", name="vstatus_enum"))
+    img_grop: Mapped[str] = mapped_column(ForeignKey("imagegroups.name"))
 
 
 class Users(Base):
@@ -51,7 +60,6 @@ class Users(Base):
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str]
     mail: Mapped[str] = mapped_column(nullable=False)
-    password_hash: Mapped[str]
     password_hash: Mapped[str]
 
 
@@ -67,6 +75,8 @@ class ClientRequest(Base):
     message: Mapped[str] = mapped_column(nullable=False)
     theme: Mapped[int] = mapped_column(ForeignKey("service.id"))
     executor: Mapped[int] = mapped_column(ForeignKey("users.id"), default=1)
+    status: Mapped[Status] = mapped_column(sqlalchemy.Enum(
+        "received", "discussed", "in progress", "closed", name="status_enum"))
 
 
 class News(Base):
@@ -77,6 +87,9 @@ class News(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     header: Mapped[str] = mapped_column(nullable=False)
     body: Mapped[str] = mapped_column(nullable=False)
+    status: Mapped[VStatus] = mapped_column(sqlalchemy.Enum(
+        "visible", "invsible", name="vstatus_enum"))
+    img_grop: Mapped[str] = mapped_column(ForeignKey("imagegroups.name"))
 
 
 if __name__ == "__main__":
