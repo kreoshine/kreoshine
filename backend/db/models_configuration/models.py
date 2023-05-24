e88e39074001b67fad93c895543c3e8d9fbf1311
pillager-SB <pillager@bk.ru>
2023-05-24 16:53:43 +0300

    fix uuid.UUID

--- backend/db/models_configuration/models.py
+++ backend/db/models_configuration/models.py
@@ -2,17 +2,61 @@
 """Module for creating a database model in Declarative Style"""
 from __future__ import annotations
 import datetime
-from uuid import uuid4
+from sqlalchemy.types import TypeDecorator, CHAR
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
 from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 
 
+class GUID(TypeDecorator):
+    """Platform-independent GUID type.
+
+    Uses PostgreSQL's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+
+    """
+
+    impl = CHAR
+    cache_ok = True
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == "postgresql":
+            return dialect.type_descriptor(UUID())
+        else:
+            return dialect.type_descriptor(CHAR(32))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == "postgresql":
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "%.32x" % uuid.UUID(value).int
+            else:
+                # hexstring
+                return "%.32x" % value.int
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                value = uuid.UUID(value)
+            return value
+
+
 class Base(DeclarativeBase):
     """
     Base class for the model
     """
+    type_annotation_map = {
+        uuid.UUID: GUID,
+    }
+
     def __tablename__(cls):
         return cls.__name__.lower()
     created_at: Mapped[datetime.datetime] = mapped_column(
@@ -24,8 +68,7 @@ class TradeItem(Base):
     Trade Items table
     """
     __tablename__ = "trade_items"
-    trade_item_uid: Mapped[str] = mapped_column(
-        default=uuid4(), primary_key=True)
+    trade_item_uid: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     description: Mapped[str] = mapped_column(nullable=False)
     short_description: Mapped[str] = mapped_column(nullable=False)
@@ -37,8 +80,7 @@ class User(Base):
     Table of database users/admins
     """
     __tablename__ = "users"
-    user_uid: Mapped[str] = mapped_column(
-        default=uuid4(), primary_key=True)
+    user_uid: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     first_name: Mapped[str] = mapped_column(nullable=False)
     last_name: Mapped[str]
     mail: Mapped[str] = mapped_column(nullable=False)
@@ -50,8 +92,7 @@ class ClientRequest(Base):
     Client card table
     """
     __tablename__ = "client_requests"
-    client_request_uid: Mapped[str] = mapped_column(
-        default=uuid4(), primary_key=True)
+    client_request_uid: Mapped[uuid.UUID] = mapped_column(primary_key=True)
     client_name: Mapped[str] = mapped_column(nullable=False)
     client_phone: Mapped[str] = mapped_column(nullable=True)
     client_mail: Mapped[str] = mapped_column(nullable=False)
