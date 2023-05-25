442841aa721373d14e84d04bea48fb01e1eb1e07
Sincov Vitalii <sincov@algont.ru>
2023-05-25 14:32:44 +0300

    refactor: move module with models to 'main' service
    create them for every service separately for independence

--- backend/db/alembic/env.py
+++ backend/db/alembic/env.py
@@ -2,10 +2,9 @@
 import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import pool
+from sqlalchemy import pool, MetaData
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
-from models_configuration import models
 from alembic import context
 
 # this is the Alembic Config object, which provides
@@ -21,7 +20,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
+target_metadata = MetaData()
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
